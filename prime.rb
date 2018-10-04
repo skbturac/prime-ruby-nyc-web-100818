e122d54628cd0b6bccef42f1ef73b8c3f0370dee
Suheyl Turac <skbsturac@live.com>
2018-10-04 03:04:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  count = 2
+  prime = true
+  if num <= 1
+    prime = false
+  else 
+    while count < num
+      if num % count == 0 
+        prime = false
+      end
+      count += 1
+    end
+  end
+ prime
+end    
