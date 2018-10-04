94b010be2db79d740739c11d1ddff81e546d4839
Suheyl Turac <skbsturac@live.com>
2018-10-04 03:01:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  i = 2
+  isPrime = true
+  if num <= 1
+    isPrime = false
+  else 
+    while i < num
+      if num % i == 0 
+        isPrime = false
+      end
+      i += 1
+    end
+  end
+  isPrime
+end    
